311f8fe475be9c373285863b20e51a494c651092
Jayanti Thakur <jayanti5thakur@gmail.com>
2024-02-20 09:52:19 +0545

    Update gcdTB.vhdl

--- GCD/gcdTB.vhdl
+++ GCD/gcdTB.vhdl
@@ -1,46 +1,46 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity gcd_tb  is 
-	end gcd_tb;
+entity fsmgcd_tb is
+end fsmgcd_tb;
 
-architecture behavior_gcd of gcd_tb is 
-signal clk, rst: std_logic;
-signal a, b, gcd: integer;
-component greatest 
-	port(clk, rst : IN STD_LOGIC;
-	    a, b: IN INTEGER;
-	    gcd : OUT INTEGER);
-END COMPONENT;
-BEGIN
-GCD_1 : greatest port map(clk=> clk, rst=>  rst, a=>a, b=>b, gcd=>gcd);
-clock: process
+architecture behaviour of fsmgcd_tb is
+
+	signal clk, reset: std_logic;
+	signal a, b, gcd: integer;
+	component fsmgcd
+		port(
+			clk, reset: in std_logic;
+			A, B: in integer;
+			GCD: out integer
+		);
+	end component;
+
 begin
-	clk<= '1';
-	wait for 100 ns;
+	fsmgcd1: fsmgcd port map(clk => clk, reset => reset, a => a, b => b, gcd => gcd);
+				 clock: process
+				 begin
+					 clk <= '1';
+					 wait for 100 ns;
+					 clk <= '0';
+					 wait for 100 ns;
+				 end process;
 
-	clk <= '0';
-	WAIT FOR 100 NS;
-	
-end process;
-process
-begin
-	rst <= '1';
-	wait for 10 ns;
-	rst  <= '0';
+				 process
+				 begin
+					 wait for 100 ns;
+					 reset <= '0';
+					 a <= 10;
+					 b <= 15;
+					
+					 wait for 2100 ns;
+					 reset <= '1';
+					 a <= 3456;
+					 b <= 234;
 
-	a <= 23;
-	b <= 11;
-	wait for 2000 ns;
-	
-	a <= 123;
-	b <= 45;
-	wait for 2000 ns;
-	
-	a <= 1112;
-	b <= 24;
-	wait for 2000 ns;
-
-	WAIT;
-end process;
-end behavior_gcd;
+					 wait for 600 ns;
+					 reset <='0';
+					 
+					 wait;
+				 end process;
+			 end behaviour;
