b7930dae7cc3ce0be469bf247288eee6178e3898
Jayanti Thakur <jayanti5thakur@gmail.com>
2024-02-20 09:45:06 +0545

    Update gcd.vhdl

--- GCD/gcd.vhdl
+++ GCD/gcd.vhdl
@@ -1,60 +1,57 @@
-ibrary  ieee;
+library ieee;
 use ieee.std_logic_1164.all;
 
-entity  greatest is
-	port(rst, clk : in std_logic;
-	    a, b : in integer;
-	    gcd : out integer);
-end greatest;
+entity fsmgcd is
+	port(
+		clk, reset: in std_logic;
+		A, B: in integer;
+		GCD: out integer
+	);
+end fsmgcd;
 
-architecture behavior_gcd of greatest is
-type state is (start, input, output, check, check1,update_x,  update_y);
-signal current_state, next_state: state;
+architecture behaviour of fsmgcd is
+	type state is (start, input, output, check, updatex, updatey);
+	signal current_state, next_state: state;
+
 begin
-state_reg:Process(clk, rst)
-BEGIN
-	IF(rst = '1') THEN
-		current_state <= start;
-	ELSIF(rising_edge(clk)) THEN
-		current_state <= next_state;
-	END IF;
-end process;
-compute:Process(a, b, current_state)
-variable x, y, r, p : integer;
-begin
-	case current_state IS 
-		WHEN start =>
-			next_state <= input;
-		WHEN input =>
-			x:= a;
-			y:= b;
-			next_state <= check;
-		WHEN check =>
-			if(x< y) THEN
-				next_state <= update_x;
-			else
-				next_state <= update_y;
-			END IF;
-      next_state <= check1;
-		WHEN  check1 =>
-			while y /= 0 loop
-				r:= x mod y;
-				x:= y;
-				y:= r;
-			end loop;
-			next_state <= output;
-		WHEN update_x =>
-			p:=x;
-			x:=y;
-			y:=p;
-		when update_y =>
-			x:=x;
-			y:=y;
-		WHEN output =>
-			GCD <= 	x;
-			next_state <= start;
-		WHEN others =>
-			next_state <= start;
-	end case;
-end process compute;
-end behavior_gcd;
+	state_register: process (clk, reset)
+	begin
+		if reset = '1' then
+			current_state <= start;
+		elsif rising_edge(clk) then
+			current_state <= next_state;
+		end if;
+	end process;
+
+	compute: process(a, b, current_state)
+		variable x, y: integer;
+	begin
+		case current_state is
+			when start =>
+				next_state <= input;
+			when input =>
+				x := a;
+				y := b;
+				next_state <= check;
+			when check =>
+				if x < y then
+					next_state <= updatey;
+				elsif y < x then
+					next_state <= updatex;
+				else
+					next_state <= output;
+				end if;
+			when updatex =>
+				x := x - y;
+				next_state <= check;
+			when updatey =>
+				y := y - x;
+				next_state <= check;
+			when output =>
+				gcd <= x;
+				next_state <= start;
+			when others =>
+				next_state <= start;
+		end case;
+	end process compute;
+end behaviour;
